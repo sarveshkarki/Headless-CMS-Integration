659d2939f63151542950e082ed65c4f3d2aa0b44
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2025-05-17 13:33:43 +0545

    updated env

--- hooks/useHomepageSection.ts
+++ hooks/useHomepageSection.ts
@@ -1,9 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const API_URL =
-  "https://sarvesh.digital6.au/index.php?rest_route=/acf/v3/pages/9";
-
 type SectionType = {
   acf_fc_layout: string;
   [key: string]: any;
@@ -13,6 +10,14 @@ export function useHomepageSections() {
   return useQuery<SectionType[], Error>({
     queryKey: ["homepage-section"],
     queryFn: async () => {
+      const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+      if (!API_URL) {
+        throw new Error(
+          "Environment variable NEXT_PUBLIC_API_URL is not defined"
+        );
+      }
+
       const response = await axios.get(API_URL);
       return response.data.acf.homepage_sections as SectionType[];
     },
